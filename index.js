8bfce252aa2d7f1f644f1a56e845acf706943a76
Jeanette Yue <yue.jeanette@gmail.com>
2020-04-10 10:47:55 -0700

    Comment out cors;

--- index.js
+++ index.js
@@ -29,22 +29,22 @@ app.get("/triggerDownload/:download", (req, res) => {
     .catch((e) => console.log(e));
 });
 
-var allowedOrigins = ["http://localhost:8081", "http://yourapp.com"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not " +
-          "allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// var allowedOrigins = ["http://localhost:8081", "http://yourapp.com"];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 app.listen(process.env.PORT || 8082);
